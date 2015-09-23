7f8a26e97133e57b7356ce789fb5124552a6a53b
U-MediaValet\sterling <sterling@STERLING-PC.hq.mediavalet.com>
2015-09-23 08:34:11 -0700

    Add compareTo function

--- key-bindings/media-valet.el
+++ key-bindings/media-valet.el
@@ -2,3 +2,4 @@
 (define-prefix-command 'mediavalet)
 (global-set-key (kbd "C-c m") 'mediavalet)
 (global-set-key (kbd "C-c m r") 'mv-escape-row-key)
+(global-set-key (kbd "C-c m c") 'mv-compare-to-pk)
--- util/media-valet.el
+++ util/media-valet.el
@@ -1,4 +1,6 @@
 ;; Functions specific to MediaValet development
+
+; Escapes a row-key, where a => b61 and b => b62
 (defun mv-escape-row-key (start end)
   (interactive "r")
   (let ((regionString (buffer-substring-no-properties start end)))
@@ -7,3 +9,22 @@
      (s-replace "a" "b61" 
 		(s-replace "b" "b62" 
 			   regionString)))))
+
+
+; Outputs a compare to string from the given region
+(defun mv-compare-to-pk (start end)
+  (interactive "r")
+  (let ((regionString (buffer-substring-no-properties start end)))
+    (delete-region start end)
+    (insert "entry.PartitionKey.CompareTo(\"")
+    (insert regionString)
+
+    (insert "\") >= 0")
+    (insert " && ")
+
+    (insert "entry.PartitionKey.CompareTo(\"")
+    (insert (s-left (- 1 (length regionString)) regionString))
+    (insert
+     (+ (string-to-char (s-right 1 regionString)) 1))
+    (insert "\") < 0")
+    ))
