547fcf9517871ab803b183395b8af6ca1402296a
U-MediaValet\Sterling.Graham <Sterling.Graham@STERLING-PC.hq.mediavalet.com>
2016-05-16 09:28:39 -0700

    Add in command to identify an image from dired mode

--- mode-configuration/dired+.el
+++ mode-configuration/dired+.el
@@ -2,3 +2,9 @@
 
 (setq dired-hide-details-initially 'nil)
 (require 'dired+)
+
+(defun direds-identify-image (arg)
+  (interactive "P")
+  (dired-do-async-shell-command "identify -verbose" current-prefix-arg (dired-get-marked-files t current-prefix-arg)))
+
+(define-key dired-mode-map (kbd ";") 'direds-identify-image)
